b296325b671229c7d21b1b9278d4c20f453f6d54
Dhananjay Pundir <129491061+Dhananjayrana09@users.noreply.github.com>
2024-01-20 03:25:48 +0530

    hero section completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
+
 const Home = () => {
   return(
     <>
 
       {/* hero Section */}
-      <> 
+       
         <section className="hero_section pt-[60px] 2xl:h-[800px]">
           <div className="container">
             <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
@@ -27,17 +31,45 @@ const Home = () => {
                 <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
 
                   <div>
-                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">30+
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">300+
                     </h2>
-                    <span className="w-[100px] h-2 bg-yellowcolor rounded-full block mt-[-14px]"></span>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                    <p className="text_para">Costumer Served</p>
                   </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">100+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                    <p className="text_para">Ambulance Available</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">250+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                    <p className="text_para">Partnerd Hospitals </p>
+                  </div>
+                </div>
+              </div>
+              {/* hero content */}
+
+
+              <div className="flex gap-[30px] justify-end">
+                <div className="mt-[30px]">
+                  <img className="w-full" src={heroImg01} alt="" />
+                </div>
+                <div>
+                  <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                  <img src={heroImg03} alt="" className="w-full"/>
                 </div>
               </div>
             </div>
           </div>
-        </section> 
+        </section>
+        {/* hero Section end */} 
       </>
-    </>
+    
   );  
 };
 
