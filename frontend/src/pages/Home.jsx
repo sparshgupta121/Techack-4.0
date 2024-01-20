f4e0e6cd825911e30c584c555eb1ae22cdcfe41c
Dhananjay Pundir <129491061+Dhananjayrana09@users.noreply.github.com>
2024-01-20 10:35:19 +0530

    testimonial comment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,8 @@ import icon02 from "../assets/images/icon02.png"
 import icon03 from "../assets/images/icon03.png"
 import {Link} from "react-router-dom";
 import {BsArrowRight} from "react-icons/bs";
+// import Testimonial from "../components/testimonial/testimonial";
+
 
 const Home = () => {
   return(
@@ -260,6 +262,23 @@ const Home = () => {
       </section>
       {/* ============== faq section end =============*/}
     
+    {/* Testimonial start */}
+
+    {/* <section>
+
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className="heading text-center">What our patience says
+          </h2>
+          <p className="text_para text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos minus perferendis labore corruption
+          </p>
+
+        </div>
+        <Testimonial/>
+      </div>
+    </section> */}
+    {/* Testimonial end */}
+
     </> 
   );  
 };
