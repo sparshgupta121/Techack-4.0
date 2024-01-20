2f15468af219c4a58bb02c4582cf3ef98f03bbf0
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-01-20 07:03:11 +0530

    text chng

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -5,13 +5,8 @@ const ServiceCard = ({ item, index }) => {
     const { name, desc, bgColor, textColor } = item;
 
     return (
-        <div className="py-[30px] px-3 lg:px-5">
-            <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
-                {name}
-            </h2>
-            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
-                {desc}
-            </p>
+        <div className="py-[0px] px-0 lg:px-0">
+           
 
             <div className="flex items-center justify-between mt-[30px]">
                 <span 
@@ -25,6 +20,12 @@ const ServiceCard = ({ item, index }) => {
                     {index + 1}
                 </span>
             </div>
+            <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
+                {name}
+            </h2>
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+                {desc}
+            </p>
         </div>
     );
 };
