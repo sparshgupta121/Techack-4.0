b7c542d744092598db0959e713a448e2e315c1d6
Ishav <ishavlega@gmail.com>
2024-01-20 07:06:10 +0530

    faq-section-completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
 import faqImg from "../assets/images/faq-img.png";
+import FaqList from "../components/Faq/FaqList";
 import About from "../components/About/about";
 import icon01 from "../assets/images/icon01.png"
 import icon02 from "../assets/images/icon02.png"
@@ -245,6 +246,15 @@ const Home = () => {
             <div className="w-1/2 hidden md:block">
               <img src={faqImg} alt="" />
             </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+
+              <FaqList />
+            </div>
+
           </div>
         </div>
       </section>
